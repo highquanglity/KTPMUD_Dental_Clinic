24a1dc82f96b13ec078c4acebc79b41774bc238d
highquanglity <nghiemquang2002@gmail.com>
2023-08-17 10:57:50 +0700

    update frontend

--- dental_clinic/management_dental_clinic/admin.py
+++ dental_clinic/management_dental_clinic/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from .models import Dentist, Patient, Staff, HealthRecord, Service, Report, DentistProcedure, BookingTicket
 # Register your models here.
-admin.site.register(Dentist)
-admin.site.register(Patient)
-admin.site.register(Staff)
-admin.site.register(HealthRecord)
-admin.site.register(Service)
-admin.site.register(Report)
-admin.site.register(DentistProcedure)
-admin.site.register(BookingTicket)
+class DentistAdmin(admin.ModelAdmin):
+    pass
+class PatientAdmin(admin.ModelAdmin):
+    pass
+class StaffAdmin(admin.ModelAdmin):
+    pass
+class HealthRecordAdmin(admin.ModelAdmin):
+    pass
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+class ReportAdmin(admin.ModelAdmin):
+    pass
+class DentistProcedureAdmin(admin.ModelAdmin):
+    pass
+class BookingTicketAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Dentist, DentistAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(HealthRecord, HealthRecordAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Report, ReportAdmin)
+admin.site.register(DentistProcedure, DentistProcedureAdmin)
